5297e3702070d48bbfe09d4934d372a88b529789
Luiz Santos <contatoluizsantosdev@gmail.com>
2026-03-31 16:05:38 -0300

    atualização da lógica

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,82 @@
-let lastCal = document.getElementById('last-calc');
-let result = document.querySelector('#result span');
-
+let lastCalc = document.getElementById('last-calc');
+let resultSpn = document.getElementById('resultSpn');
 let currentExpression = '';
 
-clearAll();
+let calculateIs = false;
 
-function insert(value) {
-    const operators = ['+', '-', '*', '/'];
+const operators = ['+', '-', '*', '/']
 
-    if (currentExpression === '' && operators.includes(value)) {
+const insert = (value) => {
+
+    //verifica para não permitir os operadores '/', '*', '+' no começo;
+    if (currentExpression === '' && operators.includes(value) && value !== '-') {
         return;
     }
 
-    let lastChar = currentExpression.slice(-1);
-
-    if (operators.includes(lastChar) && operators.includes(value)) {
-        return;
-    }
-
-    let parts = currentExpression.split(/[\+\-\*\/]/)
-    let lastNumber = parts[parts.length - 1]
-
+    //verfifica se há um '.' no número e se sim bloqueia
+    const parts = currentExpression.split(/[\+\-\*\/]/);
+    let lastNumber = parts[parts.length-1]
     if (value === '.' && lastNumber.includes('.')) {
+        return
+    }
+
+    //verifica se há um operador seguido do outro e substitui
+    let last = currentExpression.slice(-1);
+    if (operators.includes(last) && operators.includes(value)) {
+        currentExpression = currentExpression.slice(0,-1);
+        currentExpression += value;
+        lastCalc.innerHTML = currentExpression;
         return;
+    }
+
+    //Após calcular se clicar em um operador aparece o resultado da soma e continua a nova operação
+    if (calculateIs === true && !operators.includes(value)) {
+        currentExpression = value;
+        lastCalc.innerHTML = currentExpression;
+        calculateIs = false;
+        return;
+    }
+
+    //Após calcular se clicar em um número reseta a calculadora e começa uma nova operação
+    if (calculateIs === true && operators.includes(value)) {
+        currentExpression = resultSpn.innerHTML;
+        lastCalc.innerHTML = currentExpression + value;
+        currentExpression += value;
+        lastCalc.innerHTML = currentExpression;
+        calculateIs = false;
+        return;
+
     }
 
     currentExpression += value;
-    lastCal.innerHTML = currentExpression;
+    lastCalc.innerHTML = currentExpression;
 }
 
-function clearAll() {
-    lastCal.innerHTML = '0';
-    result.innerHTML = '0';
+const clearAll = () => {
+    lastCalc.innerHTML = '0';
+    resultSpn.innerHTML = '0';
     currentExpression = '';
 }
 
-function deleteLast() {
-    lastCal.innerHTML = currentExpression = currentExpression.slice(0, -1);
-    lastCal.innerHTML = currentExpression || '0';
+const calculate = () => {
+    let last = currentExpression.slice(-1);
+    calculateIs = true;
+
+    if (operators.includes(last)) {
+        resultSpn.innerHTML = 'Erro';
+        return;
+    }
+
+    let result = eval(currentExpression);
+
+    if (result === Infinity || isNaN(result) || result === -Infinity || operators.includes(last)) {
+        resultSpn.innerHTML = 'Erro';
+    } else {
+        resultSpn.innerHTML = result;
+    }
 }
 
-function calculate() {
-    try {
-        let calc = eval(currentExpression);
-        result.innerHTML = calc;
-        currentExpression = calc.toString();
-        lastCal.innerHTML = currentExpression;
-    } catch {
-        result.innerHTML = 'Erro';
-    }
+const deleteLast = () => {
+    currentExpression = currentExpression.slice(0, -1);
+    lastCalc.innerHTML = currentExpression || 0;
 }
